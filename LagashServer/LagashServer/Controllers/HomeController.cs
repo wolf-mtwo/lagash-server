1f9fab9a9829e78b536f05aac11cd12539e17ca8
wolf-mtwo <wolf.mtwo@outlook.com>
2016-09-26 20:51:06 -0400

    Adding project skeleton

---
+++ LagashServer/LagashServer/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using LagashServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LagashServer.Controllers
+{
+    public class HomeController : ApiController
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            //return new string[] { "value1", "value2" };
+            db.Students.Add(new Student { FirstMidName = "Yan", LastName = "Anand", EnrollmentDate = DateTime.Parse("2003-09-01") });
+            
+            List<Student> list = db.Students.ToList();
+            db.SaveChanges();
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LagashServer/LagashServer/Models/data.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LagashServer.Models
+{
+    public class Course
+    {
+        //[DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+
+}
