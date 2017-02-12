262721bc116414972fbac47e238f8e2690d283f1
wolf-mtwo <wolf.mtwo@outlook.com>
2017-02-12 18:24:29 -0400

    Updated book schema

--- LagashServer/Wolf.Lagash.Entities/Book.cs
+++ LagashServer/Wolf.Lagash.Entities/Book.cs
@@ -20,41 +20,62 @@ namespace Wolf.Lagash.Entities
         [Required]
         [StringLength(50)]
         public string code { get; set; }
-
+        
         [Required]
         [StringLength(36)]
         public string uuid { get; set; }
 
         [Required]
         [StringLength(250)]
-        public string titulo { get; set; }
+        public string title { get; set; }
 
         [Required]
         [StringLength(20)]
-        public string ISBN { get; set; }
+        public string isbn { get; set; }
 
         [Required]
         public bool enabled { get; set; }
 
         [Required]
+        [StringLength(250)]
         public string description { get; set; }
 
         // autors
         // specimen
 
         // optative
-        public string valumen { get; set; }
+        [StringLength(10)]
+        public string volumen { get; set; }
+
+        [StringLength(10)]
         public string tome { get; set; }
-        public string pages { get; set; }
+
+        public int pages { get; set; }
+
+        [StringLength(100)]
         public string editorial { get; set; }
+
+        [StringLength(50)]
         public string country { get; set; }
+
+        [StringLength(50)]
         public string city { get; set; }
-        //public string ISBN { get; set; }
+
+        [StringLength(100)]
         public string brings { get; set; }
-        public string year { get; set; }
+
+        public int year { get; set; }
+
+        [StringLength(50)]
         public string type { get; set; }
+        
+        [StringLength(100)]
         public string illustration { get; set; }
+
+        [StringLength(20)]
         public string size { get; set; }
+
+        [StringLength(1000)]
         public string index { get; set; }
     }
 }
