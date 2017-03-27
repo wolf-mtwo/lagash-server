d4f1d21dd8f840618fd7726e404cf147a5f9f0ad
wolf-mtwo <wolf.mtwo@outlook.com>
2017-03-26 22:43:25 -0400

    New services to support multiple libraries

---
+++ LagashServer/Wolf.Lagash.Entities/Library.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wolf.Lagash.Entities
+{
+    public class Library
+    {
+        [Key]
+        public int _id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string name { get; set; }
+    }
+}
---
+++ LagashServer/Wolf.Lagash.Interfaces/ILibrariesService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wolf.Core.Interfaces;
+using Wolf.Lagash.Entities;
+
+namespace Wolf.Lagash.Interfaces
+{
+    public interface ILibrariesService : IAdapterBase<Library>
+    {
+        bool exists(int id);
+    }
+}
