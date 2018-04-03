3f930f54427fee3172e9aba30a0eb9fbfe1fbf82
wolf-mtwo <wolf.mtwo@outlook.com>
2018-04-03 10:02:07 -0400

    Added unit test to create a book, thesis and magazine

--- LagashServer/UnitTestProject/UnitTest1.cs
+++ LagashServer/UnitTestProject/UnitTest1.cs
@@ -4,6 +4,7 @@ using Wolf.Lagash.Entities.books;
 using LagashServer.helper;
 using Wolf.Lagash.Services;
 using Wolf.Lagash.Interfaces;
+using Wolf.Lagash.Entities;
 
 namespace UnitTestProject
 {
@@ -11,12 +12,13 @@ namespace UnitTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CreateBook()
         {
             IBookService service = new BookService(new LagashContext());
-            Book item = new Book();
-            item.title = "";
-            //book.save
+            Book item = new Book() {
+                title = "book's title",
+                enabled = true
+            };
             try
             {
                 service.Create(item);
@@ -24,7 +26,47 @@ namespace UnitTestProject
             }
             catch (Exception e)
             {
-                Assert.Fail("s", e.Message);
+                Assert.Fail("hubo un problema al crear el libro", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void CreateThesis()
+        {
+            IThesisService service = new ThesisService(new LagashContext());
+            Thesis item = new Thesis()
+            {
+                title = "thesis's title",
+                enabled = true
+            };
+            try
+            {
+                service.Create(item);
+                service.Commit();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("hubo un problema al crear la tesis", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void CreateMagazine()
+        {
+            IMagazineService service = new MagazineService(new LagashContext());
+            Magazine item = new Magazine()
+            {
+                title = "magazine's title",
+                enabled = true
+            };
+            try
+            {
+                service.Create(item);
+                service.Commit();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("hubo un problema al crear la revista", e.Message);
             }
         }
     }
