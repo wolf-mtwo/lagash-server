611f83e9d235e87051fd3068a676a4ac72b56b2b
wolf-mtwo <wolf.mtwo@outlook.com>
2020-07-15 00:32:44 -0400

    New model to store readers

---
+++ LagashServer/LagashServer/Controllers/v1/helpers/reader/ReaderController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Wolf.Lagash.Services;
+using Wolf.Lagash.Entities;
+using Wolf.Lagash.Interfaces;
+using LagashServer.helper;
+using Wolf.Lagash.Entities.books;
+using LagashServer.Controllers.helpers;
+using Wolf.Lagash.Interfaces.map;
+using Wolf.Lagash.Entities.map;
+using Wolf.Lagash.Entities.tutor;
+
+namespace LagashServer.Controllers.v1.reader
+{
+    [Authorize]
+    [RoutePrefix("v1/readers")]
+    public class ReaderController : ApiController
+    {
+        private IReaderService service = new ReaderService(new LagashContext());
+        
+        [Route("")]
+        public IEnumerable<Reader> Get()
+        {
+            return service.GetAll();
+        }
+
+        [Route("")]
+        public IHttpActionResult Post(Reader item)
+        {
+            if (!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+            try {
+                service.Create(item);
+                service.Commit();
+            } catch (Exception e) {
+                return new LagashActionResult(e.Message);
+            }
+            return Ok(item);
+        }
+
+        [Route("{id}")]
+        public IHttpActionResult Get(String id)
+        {
+            Reader item = service.FindById(id);
+            if (item == null) {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+
+        [Route("{id}")]
+        public IHttpActionResult Delete(String id)
+        {
+            Reader item = service.FindById(id);
+            if (item == null) {
+                return NotFound();
+            }
+            service.Delete(item);
+            service.Commit();
+            return Ok(item);
+        }
+
+        [Route("{id}")]
+        public IHttpActionResult Put(String id, Reader item)
+        {
+            if (!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+            if (id != item._id) {
+                return new LagashActionResult("should provide a valid _id");
+            }
+            service.Update(item);
+            try {
+                service.Commit();
+            } catch (DbUpdateConcurrencyException) {
+                if (!service.exists(id)) {
+                    return NotFound();
+                } else {
+                    throw;
+                }
+            }
+            return Ok(item);
+        }
+
+        [Route("page/{page}/limit/{limit}")]
+        public IEnumerable<Reader> Get(int page, int limit)
+        {
+            return service.GetPage(page, limit, o => o.created);
+        }
+
+        [Route("page/{page}/limit/{limit}/search")]
+        public IEnumerable<Reader> GetFind(int page, int limit, string search)
+        {
+            if (search == null) search = "";
+            return service.Where(page, limit, (o) => {
+                return o.first_name.ToLower().Contains(search.ToLower()) || o.last_name.ToLower().Contains(search.ToLower());
+            }, o => o.created);
+        }
+
+        [Route("size")]
+        public Size GetSize()
+        {
+            return new Size()
+            {
+                total = service.Count()
+            };
+        }
+    }
+}
---
+++ LagashServer/Wolf.Lagash.Services/helpers/reader/ReaderService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using Wolf.Core.EntityFramework;
+using Wolf.Lagash.Entities;
+using Wolf.Lagash.Interfaces;
+
+namespace Wolf.Lagash.Services
+{
+    public class ReaderService : EFAdapterBase<Reader>, IReaderService
+    {
+        public ReaderService(DbContext Context) : base(Context)
+        {
+        }
+
+        public bool exists(String id)
+        {
+            return context.Set<Reader>().Count(e => e._id == id) > 0;
+        }
+    }
+}
