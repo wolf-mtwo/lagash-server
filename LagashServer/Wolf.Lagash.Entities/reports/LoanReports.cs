619d9b2e8fa4e1d87ccbfa957ea953960d6c2adb
wolf-mtwo <wolf.mtwo@outlook.com>
2020-08-16 23:38:35 -0400

    New model for report

---
+++ LagashServer/Wolf.Lagash.Entities/reports/LoanReports.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+s
+namespace Wolf.Lagash.Entities.reports
+{
+    public class LoanReports : Base
+    {
+        [StringLength(36)]
+        public string reader_id { get; set; }
+
+        [StringLength(36)]
+        public string resource_id { get; set; }
+
+        [StringLength(36)]
+        public string resoruce_type{ get; set; }
+
+        [StringLength(36)]
+        public string ejemplar_id { get; set; }
+
+        [StringLength(36)]
+        public string facultad_id { get; set; }
+
+        [StringLength(36)]
+        public string career_id { get; set; }
+
+        public DateTime start_date { get; set; }
+
+        public DateTime end_date { get; set; }
+    }
+}
