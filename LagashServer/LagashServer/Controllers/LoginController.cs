3309fdd4242c74a3122115e661d65980a6220e7a
Ronald Luna <ronald.ramos@iatec.com>
2020-08-18 21:41:02 -0400

    change expiration minutes by days

--- LagashServer/LagashServer/Controllers/LoginController.cs
+++ LagashServer/LagashServer/Controllers/LoginController.cs
@@ -54,7 +54,7 @@ namespace LagashServer.Controllers
             var secretKey = ConfigurationManager.AppSettings["JWT_SECRET_KEY"];
             var audienceToken = ConfigurationManager.AppSettings["JWT_AUDIENCE_TOKEN"];
             var issuerToken = ConfigurationManager.AppSettings["JWT_ISSUER_TOKEN"];
-            var expireTime = ConfigurationManager.AppSettings["JWT_EXPIRE_MINUTES"];
+            var expireDays = ConfigurationManager.AppSettings["JWT_EXPIRE_DAYS"];
 
             var securityKey = new SymmetricSecurityKey(System.Text.Encoding.Default.GetBytes(secretKey));
             var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
@@ -70,7 +70,7 @@ namespace LagashServer.Controllers
                 issuer: issuerToken,
                 subject: claimsIdentity,
                 notBefore: DateTime.UtcNow,
-                expires: DateTime.UtcNow.AddMinutes(Convert.ToInt32(expireTime)),
+                expires: DateTime.UtcNow.AddDays(Convert.ToInt32(expireDays)),
                 signingCredentials: signingCredentials);
 
             var token = tokenHandler.WriteToken(jwtSecurityToken);
