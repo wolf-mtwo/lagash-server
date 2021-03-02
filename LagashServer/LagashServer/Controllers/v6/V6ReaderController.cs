58921742608058c1de17752f8a0290a1b85f427b
wolf-mtwo <wolf.mtwo@outlook.com>
2021-03-02 09:50:07 -0400

    Adding get reader enpoint

--- LagashServer/LagashServer/Controllers/v6/V6ReaderController.cs
+++ LagashServer/LagashServer/Controllers/v6/V6ReaderController.cs
@@ -4,6 +4,10 @@ using LagashServer.helper;
 using Wolf.Lagash.Entities.helper.reader;
 using Wolf.Lagash.Services.helpers.reader;
 using Wolf.Lagash.Interfaces.helpers.reader;
+using Wolf.Lagash.Services.booking;
+using Wolf.Lagash.Interfaces.booking;
+using Wolf.Lagash.Entities.booking;
+using System.Collections.Generic;
 
 namespace LagashServer.Controllers.v6
 {
@@ -11,7 +15,8 @@ namespace LagashServer.Controllers.v6
     public class V6ReaderController : ApiController
     {
         private IReaderService service = new ReaderService(new LagashContext());
-        
+        private IBookingService service_booking = new BookingService(new LagashContext());
+
         [Route("")]
         public IHttpActionResult Post(Reader item)
         {
@@ -32,6 +37,26 @@ namespace LagashServer.Controllers.v6
         {
             Reader item = service.FindOne((o) => o.card_id == id);
             if (item == null) {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+
+        [Route("ejemplares/{ejemplar_id}")]
+        public IHttpActionResult GetReader(String ejemplar_id)
+        {
+            Booking booking = service_booking.FindOne(
+                o => o.ejemplar_id.Equals(ejemplar_id) && !o.state.Equals("RESTORED")
+            );
+
+            if (booking == null) {
+                return NotFound();
+            }
+
+            Reader item = service.FindById(booking.reader_id);
+
+            if (item == null)
+            {
                 return NotFound();
             }
             return Ok(item);
